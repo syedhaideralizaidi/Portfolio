e7816e71ddc069f2bb6251dca991d4bebbae088d
Syed Haider Ali Zaidi <xaidi.zaidi.ali@gmail.com>
2022-01-19 01:44:43 +0500

    initial commit

---
+++ base/forms.py
@@ -0,0 +1,35 @@
+from django.forms import ModelForm
+from .models import Project,Message
+
+class ProjectForm(ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ['title', 'thumbnail', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class': 'form-control'})
+
+        self.fields['body'].widget.attrs.update(
+            {'class': 'form-control', })
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = '__all__'
+        exclude = ['is_read']
+
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update(
+            {'class': 'form-control'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control', })
+
+        self.fields['subject'].widget.attrs.update(
+            {'class': 'form-control', })
+        self.fields['body'].widget.attrs.update(
+            {'class': 'form-control', })
---
+++ base/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render,redirect
+
+from .models import Project,Skill,Message
+from .forms import ProjectForm,MessageForm
+from django.contrib import messages
+# Create your views here.
+
+def homePage(request):
+
+    projects=Project.objects.all()
+    detailedSkills = Skill.objects.exclude(body='')
+    skills=Skill.objects.filter(body='')
+    form=MessageForm()
+    if request.method=='POST':
+        form=MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your Message was successfully sent!.')
+    context={'projects':projects,'skills':skills,'detailedSkills':detailedSkills,'form':form}
+    return render(request,'base/home.html',context)
+
+def projectPage(request,pk):
+    project=Project.objects.get(id=pk)
+    context = {'project':project}
+    return render(request,'base/project.html',context)
+
+def addProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/project_form.html', context)
+
+
+def editProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/project_form.html', context)
+
+def inboxPage(request):
+    inbox = Message.objects.all().order_by('is_read')
+
+    unreadCount = Message.objects.filter(is_read=False).count()
+    context = {'inbox': inbox,'unreadCount':unreadCount}
+    return render(request, 'base/inbox.html', context)
+
+
+def messagePage(request,pk):
+    message = Message.objects.get(id=pk)
+    message.is_read=True
+    message.save()
+    context = {'inbox': inbox}
+
+    return render(request, 'base/message.html', context)
